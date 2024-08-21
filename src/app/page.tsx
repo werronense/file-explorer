33a7a811c28f1a26fd7594221a3bfd12cfbf0d7d
Stephen Werronen <stephen.werronen@gmail.com>
2024-08-21 08:38:46 -0700

    Refactor to render data using state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,31 +20,36 @@ type File = {
   name: string;
   status?: string;
   fileType: string;
-}
+};
 
 type Folder = {
   id: string;
   name: string;
   files: Array<File | Folder>;
-}
+};
 
-const flattenFileTree = (tree: Array<File | Folder>, level: number): Array<[number, File | Folder]> => {
+const flattenFileTree = (
+  tree: Array<File | Folder>,
+  level: number
+): Array<[number, File | Folder]> => {
   const flattened: Array<[number, File | Folder]> = [];
 
-  tree.forEach(row => {
+  tree.forEach((row) => {
     flattened.push([level, row]);
-    
+
     if ("files" in row) {
       flattened.push(...flattenFileTree(row.files, level + 1));
     }
   });
 
   return flattened;
-}
+};
 
 export default function Home() {
   const [fileTree, setFileTree] = useState(files);
-  
+
+  const tableRows = flattenFileTree(fileTree, 1);
+
   return (
     <main className="max-w-3xl mx-auto mt-4">
       <div className="flex justify-between">
@@ -60,83 +65,51 @@ export default function Home() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">
-              <ArrowRightEndOnRectangleIcon className="size-5 inline align-text-top mr-2" />
-              Meeting Transcriptions
-            </th>
-            <td>
-              <CheckCircleIcon className="size-5 inline align-text-top mr-2 text-green-600" />
-              Healthy
-            </td>
-            <td className="text-right">
-              <ChartBarIcon className="size-5 inline align-text-top mr-2" />
-              <CodeBracketSquareIcon className="size-5 inline align-text-top mr-2" />
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">
-              <FolderIcon className="size-5 inline align-text-top mr-2" />
-              Task Management
-            </th>
-            <td></td>
-            <td className="text-right">
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" className="pl-4">
-              <ArrowRightEndOnRectangleIcon className="size-5 inline align-text-top mr-2" />
-              Capture Notes
-            </th>
-            <td>
-              <ExclamationCircleIcon className="size-5 inline align-text-top mr-2 text-amber-600" />
-              Unknown
-            </td>
-            <td className="text-right">
-              <ChartBarIcon className="size-5 inline align-text-top mr-2" />
-              <CodeBracketSquareIcon className="size-5 inline align-text-top mr-2" />
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" className="pl-4">
-              <ArrowLeftStartOnRectangleIcon className="size-5 inline align-text-top mr-2" />
-              Task Enrichment
-            </th>
-            <td>
-              <XCircleIcon className="size-5 inline align-text-top mr-2 text-red-600" />
-              Unhealthy
-            </td>
-            <td className="text-right">
-              <ChartBarIcon className="size-5 inline align-text-top mr-2" />
-              <CodeBracketSquareIcon className="size-5 inline align-text-top mr-2" />
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" className="pl-4">
-              <FolderIcon className="size-5 inline align-text-top mr-2" />
-              Utilities
-            </th>
-            <td></td>
-            <td className="text-right">
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" className="pl-8">
-              <CodeBracketIcon className="size-5 inline align-text-top mr-2" />
-              Task Lookup
-            </th>
-            <td></td>
-            <td className="text-right">
-              <ChartBarIcon className="size-5 inline align-text-top mr-2" />
-              <CodeBracketSquareIcon className="size-5 inline align-text-top mr-2" />
-              <TrashIcon className="size-5 inline align-text-top text-red-600" />
-            </td>
-          </tr>
+          {tableRows.map(([level, row]) => (
+            <tr key={row.id}>
+              <th scope="row" className={`pl-${4 * (level - 1)}`}>
+                {"files" in row && (
+                  <FolderIcon className="size-5 inline align-text-top mr-2" />
+                )}
+                {"fileType" in row &&
+                  ((row.fileType === "ingestion" && (
+                    <ArrowRightEndOnRectangleIcon className="size-5 inline align-text-top mr-2" />
+                  )) ||
+                    (row.fileType === "retrieval" && (
+                      <ArrowLeftStartOnRectangleIcon className="size-5 inline align-text-top mr-2" />
+                    )) ||
+                    (row.fileType === "code" && (
+                      <CodeBracketIcon className="size-5 inline align-text-top mr-2" />
+                    )))}
+                {row.name}
+              </th>
+              {"status" in row ? (
+                <td className="capitalize">
+                  {(row.status === "healthy" && (
+                    <CheckCircleIcon className="size-5 inline align-text-top mr-2 text-green-600" />
+                  )) ||
+                    (row.status === "unknown" && (
+                      <ExclamationCircleIcon className="size-5 inline align-text-top mr-2 text-amber-600" />
+                    )) ||
+                    (row.status === "unhealthy" && (
+                      <XCircleIcon className="size-5 inline align-text-top mr-2 text-red-600" />
+                    ))}
+                  {row.status}
+                </td>
+              ) : (
+                <td></td>
+              )}
+              <td className="text-right">
+                {!("files" in row) && (
+                  <>
+                    <ChartBarIcon className="size-5 inline align-text-top mr-2" />
+                    <CodeBracketSquareIcon className="size-5 inline align-text-top mr-2" />
+                  </>
+                )}
+                <TrashIcon className="size-5 inline align-text-top text-red-600" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
