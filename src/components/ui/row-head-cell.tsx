ce139a77a4be9ef5b065c502544617ace7a99c02
Stephen Werronen <stephen.werronen@gmail.com>
2024-08-22 16:28:56 -0700

    Add context menu

--- src/components/ui/row-head-cell.tsx
+++ src/components/ui/row-head-cell.tsx
@@ -5,6 +5,12 @@ import {
   ArrowRightEndOnRectangleIcon,
   ArrowLeftStartOnRectangleIcon,
 } from "@heroicons/react/24/outline";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
 
 type RowHeadCellProps = {
   level: number;
@@ -14,20 +20,28 @@ type RowHeadCellProps = {
 export const RowHeadCell = ({ level, file }: RowHeadCellProps) => {
   return (
     <th scope="row" style={{ paddingLeft: `${level - 1}rem`}}>
-      {"files" in file && (
-        <FolderIcon className="size-5 inline align-text-top mr-2" />
-      )}
-      {"fileType" in file &&
-        ((file.fileType === "ingestion" && (
-          <ArrowRightEndOnRectangleIcon className="size-5 inline align-text-top mr-2" />
-        )) ||
-          (file.fileType === "retrieval" && (
-            <ArrowLeftStartOnRectangleIcon className="size-5 inline align-text-top mr-2" />
-          )) ||
-          (file.fileType === "code" && (
-            <CodeBracketIcon className="size-5 inline align-text-top mr-2" />
-          )))}
-      {file.name}
+      <ContextMenu>
+        <ContextMenuTrigger>
+          {"files" in file && (
+            <FolderIcon className="size-5 inline align-text-top mr-2" />
+          )}
+          {"fileType" in file &&
+            ((file.fileType === "ingestion" && (
+              <ArrowRightEndOnRectangleIcon className="size-5 inline align-text-top mr-2" />
+            )) ||
+              (file.fileType === "retrieval" && (
+                <ArrowLeftStartOnRectangleIcon className="size-5 inline align-text-top mr-2" />
+              )) ||
+              (file.fileType === "code" && (
+                <CodeBracketIcon className="size-5 inline align-text-top mr-2" />
+              )))}
+          {file.name}
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuItem>Rename</ContextMenuItem>
+          <ContextMenuItem>Delete</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
     </th>
   );
 };
