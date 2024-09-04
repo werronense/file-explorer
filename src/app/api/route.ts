e3488da881de07dc862c233030cc69e0716c6ee6
Stephen Werronen <stephen.werronen@gmail.com>
2024-09-04 12:54:31 -0700

    Create api POST route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,22 @@
 import fs from "node:fs";
 
 export function GET() {
-    const res = fs.readFileSync(`${process.cwd()}/src/data/file-data.json`, "utf-8");
-    const data = JSON.parse(res);
-    
-    return Response.json(data);
+  const response = fs.readFileSync(
+    `${process.cwd()}/src/data/file-data.json`,
+    "utf-8"
+  );
+  const data = JSON.parse(response);
+
+  return Response.json(data);
+}
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  
+  fs.writeFileSync(
+    `${process.cwd()}/src/data/file-data.json`,
+    JSON.stringify(data)
+  );
+
+  return Response.json({ status: 200 });
 }
